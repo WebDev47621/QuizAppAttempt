26e5bdb6003db3cc65dbb5a66a8f09c458c73cb9
qat47621 <qat47621@gmail.com>
2020-11-20 14:10:51 -0800

    Added App.js, App.css, and questions.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, { useState } from 'react';
 import './App.css';
 import Display from './components/Display'
+import questions from './questions';
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const [rightAnswer, setRightAnswer] = useState(true);
+
+  const nextStep = (answer) => {
+    if (answer.correct) {
+      setRightAnswer(true);
+      setIndex(index => index + 1);
+    } else {
+      setRightAnswer(false);
+    }
+  }
+
   return (
     <div className="App">
       return (
-        <Display />
+        <Display question={questions[index]} rightAnswer={rightAnswer} nextStep={nextStep} />
       );
     </div>
   );
