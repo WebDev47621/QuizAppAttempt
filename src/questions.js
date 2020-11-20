7d315d91a456fb9645cbb7403cb9183d13106eb0
qat47621 <qat47621@gmail.com>
2020-11-20 14:14:29 -0800

    Added questions to question.js

---
+++ src/questions.js
@@ -0,0 +1,89 @@
+const questions = [
+    {
+        text: 'Test text 1',
+        answers: [
+            {
+                text: 'Answer 1',
+                correct: true;
+            },
+            {
+                text: 'Answer 2',
+                correct: false;
+            },
+            {
+                text: 'Answer 3',
+                correct: false;
+            },
+            {
+                text: 'Answer 4',
+                correct: false;
+            }
+        ]
+    },
+
+    {
+        text: 'Test text 2',
+        answers: [
+            {
+                text: 'Answer 1',
+                correct: true;
+            },
+            {
+                text: 'Answer 2',
+                correct: false;
+            },
+            {
+                text: 'Answer 3',
+                correct: false;
+            },
+            {
+                text: 'Answer 4',
+                correct: false;
+            }
+        ]
+    },
+
+    {
+        text: 'Test text 3',
+        answers: [
+            {
+                text: 'Answer 1',
+                correct: true;
+            },
+            {
+                text: 'Answer 2',
+                correct: false;
+            },
+            {
+                text: 'Answer 3',
+                correct: false;
+            },
+            {
+                text: 'Answer 4',
+                correct: false;
+            }
+        ]
+    },
+
+    {
+        text: 'Test text 4',
+        answers: [
+            {
+                text: 'Answer 1',
+                correct: true;
+            },
+            {
+                text: 'Answer 2',
+                correct: false;
+            },
+            {
+                text: 'Answer 3',
+                correct: false;
+            },
+            {
+                text: 'Answer 4',
+                correct: false;
+            }
+        ]
+    }
+];
